17dbf82966fa57fa527e0ae366a09c4e89f8d40c
root <root@KONOVALOVKIRILL.IO>
2022-06-13 01:20:04 +0000

    scrolling

--- src/app/navbar/nav.component.js
+++ src/app/navbar/nav.component.js
@@ -34,3 +34,23 @@ app.controller('headerController', function ($scope, $location) {
     return viewLocation === $location.path();
   };
 });
+
+/* smooth scrolling with a delay for routing */
+function tryScroll(href) {
+  if (!document.querySelector(href)) {
+    window.requestAnimationFrame(function () {
+      tryScroll(href);
+    });
+  } else {
+    var y =
+      $(href).offset().top - 1.6 * $('#navbarSupportedContent').outerHeight();
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    href = this.getAttribute('href');
+    tryScroll(href);
+  });
+});
