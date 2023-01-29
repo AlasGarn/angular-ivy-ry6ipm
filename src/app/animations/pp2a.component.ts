edd35391038f6fae5923013e53bcc737b8ee1b60
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 <kirillkirill0@gmail.com>
2023-01-29 02:17:26 +0000

    pp2a animation progress

--- src/app/animations/pp2a.component.ts
+++ src/app/animations/pp2a.component.ts
@@ -5,12 +5,12 @@ import { AnimationItem } from 'lottie-web';
 @Component({
   selector: 'pp2a-animation',
   templateUrl: './pp2a.component.html',
-  styles: []
+  styles: ['pp2a.component.css']
 })
 
 export class pp2aAnimationComponent {
   options: AnimationOptions = {
-    path: '../assets/animation/test.json',
+    path: '../assets/animation/pp2a_animation.json',
     autoplay: false,
     loop: false,
 	        };
@@ -24,20 +24,20 @@ export class pp2aAnimationComponent {
   @HostListener('window:scroll', ['$event']) 
   animate(event): void {
     var maxFrames = this.animationItem.firstFrame + this.animationItem.totalFrames - 1;
-    var scrollPerFrame = (this.animationBuffer - window.innerHeight / 1 ) / this.animationItem.totalFrames;
+    var scrollPerFrame = (this.animationBuffer - window.innerHeight / 1.0 ) / this.animationItem.totalFrames;
     var goTo = Math.round(window.pageYOffset) / scrollPerFrame;
-    if (goTo > maxFrames){ goTo = maxFrames };
+    if (goTo > maxFrames){ goTo = maxFrames - 1 };
     this.animationItem.goToAndStop(goTo, true);
   }
 
   animationCreated(animationItem: AnimationItem): void {
     this.animationItem = animationItem;
-    this.animationBuffer = this.animationBuffer;
+/*    this.animationBuffer = this.animationItem.totalFrames * window.innerHeight; */
   }
 
 
   ngAfterViewInit() {
-    this.animationBuffer = 2*window.innerHeight;
+    this.animationBuffer = 5 * window.innerHeight;
     this.animationParentHeight = window.innerHeight;
   }
 }
